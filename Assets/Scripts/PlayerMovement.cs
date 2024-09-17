11ad5638089d3820c194fc1366136b6c0ae3e346
Zilvinas-Jonaitis-TUS22 <k00281776@student.tus.ie>
2024-09-17 10:56:02 +0100

    Movement left and right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameInputManager InputManager;
+    public Rigidbody playerBody;
+    public int speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(InputManager.Movement.x > 0)
+        {
+            //move right
+            playerBody.velocity = new Vector3(speed,0,0);
+        }
+        if (InputManager.Movement.x < 0)
+        {
+            //move left
+            playerBody.velocity = new Vector3(-speed, 0, 0);
+        }
+        if ( InputManager.Movement.x == 0)
+        {
+            //stop
+            playerBody.velocity = new Vector3(0, 0, 0);
+        }
     }
 }
